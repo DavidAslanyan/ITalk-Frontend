c03edcc1a114d70f6de1d3796d2cdc4ccf0430a6
DavidAslanyan <david.aslanyan@icketi.com>
2025-02-06 21:45:41 +0400

    Finished menubar and tabbar development

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import LeftMenu from '../components/leftMenu';
+import Tabbar from '../components/tabbar/Tabbar';
 
 const Layout = ({ children }: Readonly<{
   children: React.ReactNode;
 }>) => {
   return (
     <main className="flex bg-backPrimary">
-      <aside className="fixed top-0 mt-2 ml-2">
+      <aside className="hidden md:block fixed top-0 mt-2 ml-2">
         <LeftMenu />
       </aside>
-      <div className='ml-[12rem] pt-2 flex-1 w-full h-auto min-h-[100vh]'>
+      <aside className='md:hidden fixed bottom-5 left-1/2 transform -translate-x-1/2'>
+        <Tabbar />
+      </aside>
+      <div className='ml-2 md:ml-[6rem] xl:ml-[12rem] pt-2 flex-1 w-full h-auto min-h-[100vh]'>
         {children}
       </div>
     </main>
---
+++ app/utilities/constants/colors.ts
@@ -0,0 +1,7 @@
+export const COLORS = {
+  white: "#ffff",
+  primary: "#DFFF5A", 
+  secondary: "#222222",
+  backPrimary: "#F0F0F0", 
+}
+
