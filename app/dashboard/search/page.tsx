432b8db46b1c3fccf175332f8925801b6b93c8bf
DavidAslanyan <david.aslanyan@icketi.com>
2025-02-08 16:25:00 +0400

    Finished initial search page

--- app/dashboard/search/page.tsx
+++ app/dashboard/search/page.tsx
@@ -1,8 +1,34 @@
-import React from 'react'
+"use client";
+import React, { useState } from 'react';
+import easyTermsData from "../../data/easy-terms.json";
+import { TermType } from '@/app/utilities/types/term.type';
+import SearchTermItem from '@/app/components/searchTermItem';
+import TermScrollable from '@/app/components/termScrollable';
+import SearchBlock from '@/app/components/searchBlock';
+
 
 const Search = () => {
+  const [termsData, setTermsData] = useState<TermType[]>(easyTermsData);
+  const [searchInput, setSearchInput] = useState<string>("");
+
   return (
-    <div>Search</div>
+    <div className='flex flex-col justify-between h-full'>
+      <div className='pt-10'>
+        <SearchBlock value={searchInput} setValue={setSearchInput} />
+      </div>
+     
+      <section className='lg:ml-10 mb-10'>
+        <TermScrollable>
+          <ul>
+            {termsData.map((term) => (
+              <li className='text-secondary' key={term.id}>
+                <SearchTermItem term={term} />
+              </li>
+            ))}
+          </ul>
+        </TermScrollable>
+      </section>
+    </div>
   )
 }
 
